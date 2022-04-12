1929b6732b4d379f4878c37818e4fb3f9f9f6035
Lyubomir Dias <lyubomirdias@gmail.com>
2022-04-12 20:05:51 +0100

    Updates

---
+++ precache-manifest.e2c56f18bba3d226511e4a8544b96491.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fdc4ebe5bbc7e5c9a85",
+    "url": "/algolia-talks/static/css/main.c46757b8.chunk.css"
+  },
+  {
+    "revision": "8fdc4ebe5bbc7e5c9a85",
+    "url": "/algolia-talks/static/js/main.8fdc4ebe.chunk.js"
+  },
+  {
+    "revision": "28ad6782969229e87e80",
+    "url": "/algolia-talks/static/js/1.28ad6782.chunk.js"
+  },
+  {
+    "revision": "15f8ccceecbceeb18d82",
+    "url": "/algolia-talks/static/js/runtime~main.15f8ccce.js"
+  },
+  {
+    "revision": "de5815fd3276d6713bf4ce39562e931c",
+    "url": "/algolia-talks/index.html"
+  }
+];
